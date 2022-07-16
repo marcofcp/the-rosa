eb239c539e1d40f71220e223c11172a08bdbce3e
marcofcp <marco_fcp85@hotmail.com>
2022-07-16 15:26:51 +0100

    scrollreveal effects update

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-//Select element function
+/* Efeitos com ScrollReveal */
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.welcome', {duration: 1000});
+sr.reveal('.the-rosa', {duration: 3000});
+sr.reveal('.divisoria, .opened', {duration: 4000});
+
+sr.reveal('.x-rotate', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2000,
+});
+
+sr.reveal('.x-y-rotate', {
+    rotate: {x:100, y:100, z:0},
+    duration: 2000,
+});
